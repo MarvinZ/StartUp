7a7ad141675c276172da14ede4eab3131832a494
Marvin Zumbado <marvinzzz@gmail.com>
2018-01-25 21:22:49 -0600

    First

---
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+
+import { DropdownModule } from 'primeng/primeng';
+
+
+
+import {
+    TimelineComponent,
+    NotificationComponent,
+    ChatComponent
+} from './components';
+import { StatModule } from '../../shared';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        NgbCarouselModule.forRoot(),
+        NgbAlertModule.forRoot(),
+        DashboardRoutingModule,
+        StatModule,
+        DropdownModule
+    ],
+    declarations: [
+        DashboardComponent,
+        TimelineComponent,
+        NotificationComponent,
+        ChatComponent
+    ]
+})
+export class DashboardModule { }
